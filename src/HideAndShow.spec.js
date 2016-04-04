a7708d731688b45e4cb908580cb3b193bec31d26
yangli1990 <yangli1990@live.com.au>
2016-04-04 17:08:11 +1000

    add hideandshow test

--- src/HideAndShow.spec.js
+++ src/HideAndShow.spec.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import HideAndShow from './HideAndShow';
+import TestUtils from 'react-addons-test-utils';
+
+describe('HideAndShow', () => {
+	it('should show child components when hide=false', () => {
+		const renderedComponent = TestUtils.renderIntoDocument(
+			<HideAndShow hide={false}>
+				<p>This should show</p>
+			</HideAndShow>);
+
+		const findParagraph = TestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'p');
+		expect(findParagraph.textContent).to.equal('This should show');
+	});
+});
